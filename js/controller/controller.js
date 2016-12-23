4e468a12622e765b190a15a8078d9318e0b9d858
ankverma <vermaankit84@gmail.com>
2016-12-23 11:48:52 -0600

    Initial Commit

---
+++ js/controller/controller.js
@@ -0,0 +1,4 @@
+var app = angular.module('myApp' , []);
+app.controller('customersCtrl', function($scope, $http) {
+    $http.get("http://www.w3schools.com/angular/customers.php").then(function (response) {$scope.names = response.data.records;});
+});
